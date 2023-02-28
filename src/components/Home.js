bb4efc009c1adc14ea5a6b98c887fe3965a569d6
idilipsarker <dilipsarker719@gmail.com>
2023-02-28 19:29:26 +0600

    user management system implemented

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,19 @@ import { useLoaderData } from "react-router-dom";
 
 const Home = () => {
   const users = useLoaderData();
+  const handleDelete = (user) => {
+    const agree = window.confirm(
+      `Are you sure you want to delete: ${user.name}`
+    );
+    console.log("deleting user with id:", user.id);
+  };
   return (
     <div>
       <h3>Users:{users.length} </h3>
       {users.map((user) => (
         <p key={user._id}>
-          Name: {user.name} Email: {user.email} <button onClick>Delete</button>
+          Name: {user.name} Email: {user.email}{" "}
+          <button onClick={() => handleDelete(user)}>Delete</button>
         </p>
       ))}
     </div>
